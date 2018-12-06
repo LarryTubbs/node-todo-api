340c3a31a27ad376dfc10b971429c7a4375f645c
LarryTubbs <larry.tubbs@gmail.com>
2018-12-06 13:18:01 -0600

    add POST /users/login route

--- server/models/User.js
+++ server/models/User.js
@@ -69,6 +69,26 @@ UserSchema.statics.findByToken = function (token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    // find a user that matches email
+    var User = this;
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, success) => {
+                if(success) {
+                    resolve(user);
+                } else {
+                    reject('no user found');
+                };
+            })
+        });
+    });
+};
+
 UserSchema.pre('save', function (next) {
     var user = this;
     if (user.isModified('password')) {
